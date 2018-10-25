8c8a928b18552c1d1381a921e1aee32c0c50570a
Przemek Recha <przemek.recha@gmail.com>
2018-10-25 16:24:27 +0200

    creating classes

--- easycpp.py
+++ easycpp.py
@@ -29,7 +29,7 @@ def selectFolderAndDownload(files, templateName):
                 folder = "{}/{}".format(os.getcwd(), args.dir)
             downloadTemplate(files, templateName, folder)
         else:
-            downloadTemplate(files, templateName, os.getcwd)
+            downloadTemplate(files, templateName, os.getcwd())
     except:
         print("folder and download: {}".format(sys.exc_info()[0]))
 
@@ -41,10 +41,30 @@ def downloadTemplate(files, templateName, folder):
             res = urllib.request.urlopen("{}/templates/project/{}".format(baseUrl, f))
             open("{}/{}".format(folder, files["templates"][templateName][f]), "wb").write(res.read())
     except:
-        print(" download: {}".format(sys.exc_info()[0]))
+        print("download: {}".format(sys.exc_info()[0]))
 
 def createClass():
-    
+    try:
+        res = urllib.request.urlopen("{}/templates/classes/files.json".format(baseUrl)) 
+        data = json.loads(res.read().decode("utf-8"))
+        print("Class templates:")
+        i = 1
+        for c in data:
+            print("{} - {}".format(i,c))
+            i = i + 1
+        selected = list(data.keys())[int(input("Select class[1-{}]: ".format(i-1)))-1]
+        className = input("Enter class name: ")
+        if os.path.isfile("Makefile"):
+            folder = os.getcwd()
+            for f in data[selected]:
+                res = urllib.request.urlopen("{}/templates/classes/{}/{}".format(baseUrl, selected, f))
+                val = res.read().decode("utf-8")
+                val = val.replace("easyclass", className)
+                fileName = "{}/{}/{}.{}".format(folder, data[selected][f]["folder"], className, data[selected][f]["extension"])
+                open(fileName, "w").write(val)
+
+    except:
+        print("create class: {}".format(sys.exc_info()[0]))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -54,7 +74,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.project:
         print("Creating easycpp project")
-        #createProject()
+        createProject()
     elif args.createClass:
         print("Creating class")
         createClass()
