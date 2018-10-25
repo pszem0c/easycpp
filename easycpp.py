e0b74e72b190cb54c88b9828af963977e01c44f6
Przemek Recha <przemek.recha@gmail.com>
2018-10-25 14:26:57 +0200

    added templates for project

--- easycpp.py
+++ easycpp.py
@@ -2,16 +2,39 @@
 import urllib.request
 import json
 import sys
+import os
 
-baseUrl = "https://raw.githubusercontent.com/acharluk/easy-cpp-projects/master"
+baseUrl = "https://raw.githubusercontent.com/pszem0c/easycpp/07db17b8caf7be75087f7ce04eed4143e3e4956b"
 
 def createProject():
     try:
         res = urllib.request.urlopen("{}/templates/project/files.json".format(baseUrl)) 
-        data = res.read().decode("utf-8")
-        print(data)
+        data = json.loads(res.read().decode("utf-8"))
+        templates = list(data["templates"].keys())
+        selected = 0 # other templates?
+        selectedTemplate = templates[0]
+        selectFolderAndDownload(data, selectedTemplate)
     except:
         print("createProject: {}".format(sys.exc_info()[0]))
+
+def selectFolderAndDownload(files, templateName):
+    try:
+        # other dirs?
+        downloadTemplate(files, templateName, os.getcwd())
+    except:
+        print("folder and download: {}".format(sys.exc_info()[0]))
+
+def downloadTemplate(files, templateName, folder):
+    try:
+        #for directory in files["directories"]:
+        #    os.mkdir("{}/{}".format(folder, directory))
+        for f in files["templates"][templateName]:
+            print(f)
+
+    except:
+        print(" download: {}".format(sys.exc_info()[0]))
+
+
 
 if __name__ == "__main__":
     createProject()
